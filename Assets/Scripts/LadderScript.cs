e057e6486585a274a8c1aad1a8d60b328889d57b
pguardia-dam <pguardia.dam@institutcampalans.net>
2025-11-22 07:23:16 +0100

    merge error layout

--- Assets/Scripts/LadderScript.cs
+++ Assets/Scripts/LadderScript.cs
@@ -2,6 +2,19 @@ using UnityEngine;
 
 public class LadderScript : MonoBehaviour
 {
+    private void Awake()
+    {
+        float height = GetComponent<SpriteRenderer>().size.y;
+        float width = GetComponent<SpriteRenderer>().size.x;
+        Transform topHandler = transform.GetChild(0).transform;
+        Transform bottomHandler = transform.GetChild(1).transform;
+        topHandler.position = new Vector3(transform.position.x, transform.position.y + (height / 2), 0);
+        bottomHandler.position = new Vector3(transform.position.x, transform.position.y - (height / 2), 0);
+        GetComponent<BoxCollider2D>().offset = Vector2.zero;
+        GetComponent<BoxCollider2D>().size = new Vector2(width, height);
+    }
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
