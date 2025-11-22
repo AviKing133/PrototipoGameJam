b5a5a53762744db318121fd854f9d403ef4ba233
pguardia-dam <pguardia.dam@institutcampalans.net>
2025-11-22 07:56:06 +0100

    merge cambio de compilacion

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.U2D.IK;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -11,6 +13,8 @@ public class PlayerMovement : MonoBehaviour
     private bool facingRight = true;
     private bool isDash = false;
     private bool canDash = true;
+
+
     [SerializeField]
     private GameObject cuadradoInteraccion;
     [SerializeField]
@@ -20,6 +24,15 @@ public class PlayerMovement : MonoBehaviour
     public float dashSpeed = 15f;
     public float dashDuration = 0.2f;
     public float dashCooldown = 1f;
+
+    // VARIABLES PARA ESCALERA DE MANO
+    private bool isUnderPlatform = false,
+    isCloseToLadder = false,
+    climbHeld = false, 
+    hasStartedClimb = false;
+    private Transform ladder;
+    private float vertical = 0f;
+    private float climbSpeed = 0.2f;
 
     void Start()
     {
@@ -69,13 +82,81 @@ public class PlayerMovement : MonoBehaviour
         {
             Flip();
         }
-    }
 
+        // LADDER MOVEMENT
+        /*
+        vertical = Input.GetAxisRaw("Vertical") * climbSpeed;
+        if (isOnGround() && horizontal.Equals(0) && !isCloseToLadder && (crouchHeld || isUnderPlatform))
+            GetComponent<Animator>().Play("CharacterCrouchIdle");
+        else if (isOnGround() && !isCloseToLadder && (horizontal > 0 || horizontal < 0) && (crouchHeld || isUnderPlatform))
+            GetComponent<Animator>().Play("CharacterCrouch");
+        else if (isOnGround() && !hasStartedClimb && horizontal.Equals(0))
+            GetComponent<Animator>().Play("CharacterIdle");
+        else if (isOnGround() && !hasStartedClimb && (horizontal > 0 || horizontal < 0))
+            GetComponent<Animator>().Play("CharacterWalk");
+        */
+        // Evalua una condicio i li assigna true si és verdader, false si és fals
+        climbHeld = (isCloseToLadder && Input.GetButton("Climb")) ? true : false;
+
+        if (climbHeld)
+        {
+            if (!hasStartedClimb) hasStartedClimb = true;
+        }
+        else
+        {
+            if (hasStartedClimb)
+            {
+                GetComponent<Animator>().Play("CharacterClimbIdle");
+            }
+        }
+    }
+    /*
+    void FixedUpdate()
+    {
+    // Climbing
+    if (hasStartedClimb && !climbHeld)
+        {
+            if (horizontal > 0 || horizontal < 0) ResetClimbing();
+        }
+        else if (hasStartedClimb && climbHeld)
+        {
+            float height = GetComponent<SpriteRenderer>().size.y;
+            float topHandlerY = Half(ladder.transform.GetChild(0).transform.position.y + height);
+            float bottomHandlerY = Half(ladder.transform.GetChild(1).transform.position.y + height);
+            float transformY = Half(transform.position.y);
+            float transformVY = transformY + vertical;
+
+            if (transformVY > topHandlerY || transformVY < bottomHandlerY)
+            {
+                ResetClimbing();
+            }
+            else if (transformY <= topHandlerY && transformY >= bottomHandlerY)
+            {
+                rigidBody2D.bodyType = RigidbodyType2D.Kinematic;
+                if (!transform.position.x.Equals(ladder.transform.position.x))
+                    transform.position = new Vector3(ladder.transform.position.x, transform.position.y, transform.position.z);
+
+                GetComponent<Animator>().Play("CharacterClimb");
+                Vector3 forwardDirection = new Vector3(0, transformVY, 0);
+                Vector3 newPos = Vector3.zero;
+                if (vertical > 0)
+                    newPos = transform.position + forwardDirection * Time.deltaTime * climbSpeed;
+                else if (vertical < 0)
+                    newPos = transform.position - forwardDirection * Time.deltaTime * climbSpeed;
+                if (newPos != Vector3.zero) rigidBody2D.MovePosition(newPos);
+            }
+        }
+    */
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("PICKUP"))
         {
             cuadradoInteraccion.SetActive(true);
+        }
+        if (collision.gameObject.tag.Equals("Ladder"))
+        {
+            isCloseToLadder = true;
+            this.ladder = collision.transform;
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
