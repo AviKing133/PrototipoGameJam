8c1f9504738b0235906a30ab3c0c508729a94e87
pguardia-dam <pguardia.dam@institutcampalans.net>
2025-11-23 13:00:25 +0100

    merge casi todo hecho

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,34 +5,64 @@ public class GameManager : MonoBehaviour
     public bool playerHaveDash = false;
     public bool playerWallJump = false;
 
-    static public GameManager instance;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public void Awake()
+    public static GameManager instance;
+
+    [SerializeField] private AudioClip dashClip;
+    [SerializeField] private AudioClip wallJumpClip;
+    [SerializeField] private AudioClip jumpClip;
+    [SerializeField] private AudioClip damageClip;
+    [SerializeField] private AudioClip pickupClip;
+
+    private AudioSource audioSource;
+
+    void Awake()
     {
         if (instance == null)
         {
             instance = this;
             DontDestroyOnLoad(gameObject);
+
+            audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.playOnAwake = false;
         }
         else
         {
             Destroy(gameObject);
         }
     }
-    void Start()
-    {
 
-    }
-    void Update()
-    {
-
-    }
     public void ActivateDash()
     {
         playerHaveDash = true;
+        PlaySound(dashClip);
     }
+
     public void ActivateWallJump()
     {
         playerWallJump = true;
+        PlaySound(wallJumpClip);
+    }
+
+    public void PlayerJump()
+    {
+        PlaySound(jumpClip);
+    }
+
+    public void PlayerDamage()
+    {
+        PlaySound(damageClip);
+    }
+
+    public void PlayerPickup()
+    {
+        PlaySound(pickupClip);
+    }
+
+    private void PlaySound(AudioClip clip)
+    {
+        if (clip != null)
+        {
+            audioSource.PlayOneShot(clip);
+        }
     }
 }
